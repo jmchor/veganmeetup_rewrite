4319bb023403e7c6504b43b00b05ee0ae4db00d8
Johannes Chorzempa <jchorzempa@posteo.org>
2024-06-20 06:22:12 +0200

    create GlobalStyles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+
+html {
+	--red: #ff0000;
+	--blue: #61A7DB;
+	--lightBlue: #B6E2F6 ;
+	--lighterGrey: #DCE2E5;
+
+	/* --maxwidth: 1000px; */
+	--bs: 0 12px 24px 0 rgba(0,0,0,0.09);
+	box-sizing: border-box;
+	/* font-size: 62.5%; */
+}
+
+*, *:before, *:after {
+	box-sizing: inherit;
+}
+
+body {
+	margin: 0;
+    padding:0;
+	background-image: linear-gradient(60deg, #64b3f4 0%, #c2e59c 100%);
+    height: 100vh;
+
+
+    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif
+}
+
+
+
+`;
