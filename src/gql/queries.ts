5bac0fa5fdf25b3f6fdd2376a9d8d235e54ea123
Johannes Chorzempa <jchorzempa@posteo.org>
2024-06-24 09:55:07 +0200

    add Events query

--- src/gql/queries.ts
+++ src/gql/queries.ts
@@ -1,12 +1,31 @@
 import { graphql } from 'gql.tada';
 
 export const ALL_IMAGES_QUERY = graphql(`
-	query ImageQuery($where: ImageWhereInput!) {
+	query ALL_IMAGES_QUERY($where: ImageWhereInput!) {
 		images(where: $where) {
 			image {
 				publicUrlTransformed
 			}
 			alt
+			id
+		}
+	}
+`);
+
+export const ALL_EVENTS_QUERY = graphql(`
+	query ALL_EVENTS_QUERY($orderBy: [EventOrderByInput!]!) {
+		events(orderBy: $orderBy) {
+			id
+			title
+			date
+			from
+			until
+			pastEvent
+			location
+			content {
+				document
+			}
+			timestamp
 		}
 	}
 `);
