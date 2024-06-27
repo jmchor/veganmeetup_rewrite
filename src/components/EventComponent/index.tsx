5346f1c610b74dbe4c82269f356f724e03d0af8e
Johannes Chorzempa <jchorzempa@posteo.org>
2024-06-27 06:26:55 +0200

    create style

--- src/components/EventComponent/index.tsx
+++ src/components/EventComponent/index.tsx
@@ -1,8 +1,7 @@
 import { useQuery } from '@apollo/client';
-import { Link } from '@tanstack/react-router';
 import { ALL_EVENTS_QUERY } from 'src/gql/queries';
 import transformDateToArray from 'src/lib/transformDateToArray';
-import styled from 'styled-components';
+import { ComponentContainer, ContentContainer, DateContainer } from './styles';
 
 const EventComponent = () => {
 	const { data } = useQuery(ALL_EVENTS_QUERY, {
@@ -44,82 +43,3 @@ const EventComponent = () => {
 	);
 };
 export default EventComponent;
-
-export const ComponentContainer = styled(Link)`
-	display: flex;
-	height: 100%;
-	width: 100%;
-	text-decoration: none;
-	color: black;
-	@media screen and (max-width: 900px) {
-		flex-direction: column;
-	}
-`;
-
-export const DateContainer = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: auto;
-	height: 100%;
-	flex: 1;
-	gap: 30px;
-	background-color: #9cdff3;
-
-	flex-direction: column;
-
-	h1 {
-		font-size: 50px;
-	}
-
-	h1,
-	h4 {
-		margin: 0;
-	}
-
-	@media screen and (max-width: 900px) {
-		flex-direction: row;
-		gap: 10px;
-		padding: 5px;
-		h1 {
-			font-size: 20px;
-		}
-	}
-`;
-
-export const ContentContainer = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: auto;
-	height: 100%;
-	flex: 2.5;
-	gap: 20px;
-	background-color: #f4fcff;
-
-	flex-direction: column;
-
-	h1,
-	h4 {
-		margin: 0;
-	}
-
-	@media screen and (max-width: 900px) {
-		gap: 10px;
-		padding: 5px;
-		h1 {
-			font-size: 20px;
-		}
-
-		p {
-			margin: 0;
-		}
-	}
-`;
-
-export const EventLink = styled(Link)`
-	text-decoration: none;
-	color: black;
-	margin: 0;
-	padding: 0;
-`;
---
+++ src/components/EventComponent/styles.ts
@@ -0,0 +1,81 @@
+import { Link } from '@tanstack/react-router';
+import styled from 'styled-components';
+
+export const ComponentContainer = styled(Link)`
+	display: flex;
+	height: 100%;
+	width: 100%;
+	text-decoration: none;
+	color: black;
+	@media screen and (max-width: 900px) {
+		flex-direction: column;
+	}
+`;
+
+export const DateContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: auto;
+	height: 100%;
+	flex: 1;
+	gap: 30px;
+	background-color: #9cdff3;
+
+	flex-direction: column;
+
+	h1 {
+		font-size: 50px;
+	}
+
+	h1,
+	h4 {
+		margin: 0;
+	}
+
+	@media screen and (max-width: 900px) {
+		flex-direction: row;
+		gap: 10px;
+		padding: 5px;
+		h1 {
+			font-size: 20px;
+		}
+	}
+`;
+
+export const ContentContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: auto;
+	height: 100%;
+	flex: 2.5;
+	gap: 20px;
+	background-color: #f4fcff;
+
+	flex-direction: column;
+
+	h1,
+	h4 {
+		margin: 0;
+	}
+
+	@media screen and (max-width: 900px) {
+		gap: 10px;
+		padding: 5px;
+		h1 {
+			font-size: 20px;
+		}
+
+		p {
+			margin: 0;
+		}
+	}
+`;
+
+export const EventLink = styled(Link)`
+	text-decoration: none;
+	color: black;
+	margin: 0;
+	padding: 0;
+`;
