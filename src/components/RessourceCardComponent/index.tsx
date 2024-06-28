7345082f67aa890ab8fb6e4fb71fe129ff777045
Johannes Chorzempa <jchorzempa@posteo.org>
2024-06-28 09:59:22 +0200

    create ressourcecardcomponent

--- src/components/RessourceCardComponent/index.tsx
+++ src/components/RessourceCardComponent/index.tsx
@@ -1,19 +1,31 @@
-const RessourceCardComponent = ({ placeholder }: { placeholder: string }) => {
+import { CardContainer, Upper, Lower } from './styles';
+
+const RessourceCardComponent = ({
+	url,
+	image,
+	name,
+	description,
+	author,
+}: {
+	url: string;
+	image: string;
+	name: string;
+	description?: string;
+	author?: string;
+}) => {
 	return (
-		<div
-			style={{
-				width: '100%',
-				height: '400px',
-				border: '1px solid black',
-				display: 'flex',
-				justifyContent: 'center',
-				alignItems: 'center',
-				background: '#fcfcfc',
-				padding: '10px',
-			}}
-		>
-			<p>{placeholder}</p>
-		</div>
+		<CardContainer style={{}}>
+			<Upper>{<img src={image} alt='image' />}</Upper>
+			<Lower>
+				<a href={url}>
+					<h1>{name}</h1>
+				</a>
+
+				{author && <i>von {author}</i>}
+
+				{description && <p>{description}</p>}
+			</Lower>
+		</CardContainer>
 	);
 };
 export default RessourceCardComponent;
