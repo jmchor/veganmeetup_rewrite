5fff268957b3085cc15bf55db7780736cdc4d71a
Johannes Chorzempa <jchorzempa@posteo.org>
2024-06-28 17:43:34 +0200

    Rewrite index page & import styles

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,90 +1,54 @@
-import { Link, createFileRoute } from '@tanstack/react-router';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import EmbeddedPageComponent from 'src/components/EmbeddedPageComponent';
-
-import styled from 'styled-components';
+import { createFileRoute } from '@tanstack/react-router';
+import {
+	AboutLinkContainer,
+	HistoryLinkContainer,
+	LowerLinkContainer,
+	ResourceLinkContainer,
+	UpperLinkContainer,
+	VeganMeetupLinkContainer,
+} from 'src/styles/IndexPageStyles';
+import { Headline, MainContainer, PageContainer } from 'src/styles/VeganMeetupRouteStyles';
 
 export const Route = createFileRoute('/')({
 	component: LandingPage,
 });
 
 function LandingPage() {
-	const responsive = {
-		superLargeDesktop: {
-			// the naming can be any, depends on you.
-			breakpoint: { max: 4000, min: 3000 },
-			items: 1,
-		},
-		desktop: {
-			breakpoint: { max: 3000, min: 1024 },
-			items: 1,
-		},
-		tablet: {
-			breakpoint: { max: 1024, min: 464 },
-			items: 1,
-		},
-		mobile: {
-			breakpoint: { max: 464, min: 0 },
-			items: 1,
-		},
-	};
 	return (
 		<>
-			<CarouselContainer>
-				<CarouselStyles responsive={responsive}>
-					<Container>
-						<EmbeddedPageComponent source='http://localhost:5173/veganmeetup' />
-						<OverlayLink to='http://localhost:5173/veganmeetup'></OverlayLink>
-					</Container>
-					<Container>
-						<EmbeddedPageComponent source='http://localhost:5173/about' />
-						<OverlayLink to='http://localhost:5173/about'></OverlayLink>
-					</Container>
-					<Container>
-						<EmbeddedPageComponent source='http://localhost:5173/timeline' />
-						<OverlayLink to='http://localhost:5173/timeline'></OverlayLink>
-					</Container>
-					<Container>
-						<EmbeddedPageComponent source='http://localhost:5173/resources' />
-						<OverlayLink to='http://localhost:5173/resources'></OverlayLink>
-					</Container>
-				</CarouselStyles>
-			</CarouselContainer>
+			<PageContainer>
+				<MainContainer>
+					<UpperLinkContainer className='upperContainer'>
+						<HistoryLinkContainer to='/timeline'>
+							<Headline>Geschichte</Headline>
+						</HistoryLinkContainer>
+						<VeganMeetupLinkContainer to='/veganmeetup'>
+							<Headline>Vegan Meetup Mainz</Headline>
+						</VeganMeetupLinkContainer>
+					</UpperLinkContainer>
+
+					<LowerLinkContainer className='lowerContainer'>
+						<AboutLinkContainer to='/about'>
+							<Headline>Über uns</Headline>
+						</AboutLinkContainer>
+
+						<ResourceLinkContainer to='/resources'>
+							<Headline>Ressourcen</Headline>
+						</ResourceLinkContainer>
+					</LowerLinkContainer>
+					<div
+						style={{
+							height: 'auto',
+							margin: '0 20px 20px 20px',
+							backgroundColor: 'black',
+							padding: '10px',
+							borderRadius: '10px',
+						}}
+					>
+						<p style={{ textAlign: 'center', margin: '0', color: 'white' }}>© jxor 2024</p>
+					</div>
+				</MainContainer>
+			</PageContainer>
 		</>
 	);
 }
-
-export const Container = styled.div`
-	height: 80vh;
-	width: 80%;
-	padding: 10px;
-	margin: 0 auto;
-	background: #fcfcfc81;
-	position: relative;
-	overflow-y: hidden;
-
-	@media screen and (max-width: 768px) {
-		width: 90%;
-		height: 90vh;
-	}
-`;
-
-export const CarouselContainer = styled.div`
-	margin-top: 3rem;
-`;
-
-export const CarouselStyles = styled(Carousel)`
-	box-shadow: 0px 200px 100px -180px rgba(0, 0, 0, 0.293);
-	button {
-		background: rgb(108 105 105);
-	}
-`;
-
-export const OverlayLink = styled(Link)`
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-`;
---
+++ src/styles/IndexPageStyles.ts
@@ -0,0 +1,106 @@
+import { Link } from '@tanstack/react-router';
+import styled from 'styled-components';
+
+export const UpperLinkContainer = styled.div`
+	height: 100%;
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	margin-bottom: 20px;
+	flex: 1;
+
+	@media screen and (max-width: 768px) {
+		flex-direction: column;
+		margin-bottom: 0px;
+	}
+`;
+
+export const LowerLinkContainer = styled.div`
+	height: 100%;
+	display: flex;
+	flex: 1;
+
+	@media screen and (max-width: 768px) {
+		flex-direction: column-reverse;
+		justify-content: center;
+		align-items: center;
+	}
+`;
+
+export const HistoryLinkContainer = styled(Link)`
+	border-radius: 10px;
+	text-decoration: none;
+	background-color: #5978f7;
+	color: white;
+	font-size: 30px;
+	display: flex;
+	height: auto;
+	width: 33%;
+	justify-content: center;
+	align-items: center;
+	padding: 20px;
+
+	margin: 20px 20px 0 20px;
+
+	@media screen and (max-width: 768px) {
+		width: 90%;
+		margin-top: 20px;
+	}
+`;
+
+export const VeganMeetupLinkContainer = styled(Link)`
+	border-radius: 10px;
+	text-decoration: none;
+	background-color: #12b6e3fb;
+	display: flex;
+	flex-direction: column;
+	height: auto;
+	justify-content: center;
+	align-items: center;
+	flex: 1;
+	padding: 20px;
+	margin: 20px 20px 0 0px;
+
+	@media screen and (max-width: 768px) {
+		width: 90%;
+		margin: 20px;
+	}
+`;
+
+export const AboutLinkContainer = styled(Link)`
+	border-radius: 10px;
+	background-color: #9d9d9ddf;
+	text-decoration: none;
+	display: flex;
+	height: auto;
+	width: 80%;
+	justify-content: center;
+	align-items: center;
+	padding: 20px;
+	margin: 0 20px 20px 20px;
+	flex: 1;
+
+	@media screen and (max-width: 768px) {
+		width: 90%;
+		height: 280px;
+	}
+`;
+
+export const ResourceLinkContainer = styled(Link)`
+	border-radius: 10px;
+	text-decoration: none;
+	display: flex;
+	flex-direction: column;
+	height: auto;
+	width: 35%;
+	justify-content: center;
+	align-items: center;
+	background-color: #b300ffdf;
+	margin: 0px 20px 20px 0;
+
+	@media screen and (max-width: 768px) {
+		width: 90%;
+		height: 280px;
+		margin: 0px 20px 20px 20px;
+	}
+`;
