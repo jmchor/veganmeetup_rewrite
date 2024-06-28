4a893208a466416c26b6abd922091729c3d1632c
Johannes Chorzempa <jchorzempa@posteo.org>
2024-06-28 10:01:47 +0200

    create resource page

--- src/routes/ressources.tsx
+++ src/routes/ressources.tsx
@@ -1,31 +1,80 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { useQuery } from '@apollo/client';
+import { Link, createFileRoute } from '@tanstack/react-router';
+import { useState } from 'react';
+import { MdKeyboardDoubleArrowLeft } from 'react-icons/md';
 import { ResponsiveMasonry } from 'react-responsive-masonry';
 import RessourceCardComponent from 'src/components/RessourceCardComponent';
-import { MasonryContainer, MasonryStyles, PageContainer } from 'src/styles/VeganMeetupRouteStyles';
-import styled from 'styled-components';
+import { ALL_ACTIVISTS_QUERY, ALL_BOOKS_QUERY, ALL_PODCASTS_QUERY, ALL_VIDEOS_QUERY } from 'src/gql/queries';
+import { RessourceNavigation, AnnouncementContainer, RessourceMasonryStyles } from 'src/styles/ResourceRouteStyles';
+import { MasonryContainer, PageContainer } from 'src/styles/VeganMeetupRouteStyles';
 
 export const Route = createFileRoute('/ressources')({
 	component: RessourcesPage,
 });
 
+interface BaseResource {
+	id: string | null;
+	url: string | null;
+	thumbnail: { image: { publicUrlTransformed: string } } | null;
+	name: string | null;
+	description: string | null;
+	author?: string | null;
+}
+
 function RessourcesPage() {
-	const array = [1, 2, 3, 4, 5, 6, 7, 5, 6, 3, 34, 1, 2, 3, 2];
+	const [allDataArray, setAllDataArray] = useState<BaseResource[]>([]);
+	const { data: activistData } = useQuery(ALL_ACTIVISTS_QUERY, {
+		onCompleted: (data) => {
+			setAllDataArray((prev) => [...prev, ...(data?.activists as BaseResource[])] as BaseResource[]);
+		},
+	});
+	const { data: bookData } = useQuery(ALL_BOOKS_QUERY, {
+		onCompleted: (data) => {
+			setAllDataArray((prev) => [...prev, ...(data?.books as BaseResource[])] as BaseResource[]);
+		},
+	});
+
+	const { data: podcastData } = useQuery(ALL_PODCASTS_QUERY, {
+		onCompleted: (data) => {
+			setAllDataArray((prev) => [...prev, ...(data?.podcasts as BaseResource[])] as BaseResource[]);
+		},
+	});
+	const { data: videoData } = useQuery(ALL_VIDEOS_QUERY, {
+		onCompleted: (data) => {
+			setAllDataArray((prev) => [...prev, ...(data?.videos as BaseResource[])] as BaseResource[]);
+		},
+	});
+
 	return (
-		<PageContainer style={{ overflow: 'scroll', scrollbarWidth: 'none' }}>
+		<PageContainer style={{ overflow: 'scroll', scrollbarWidth: 'none', flexDirection: 'column' }}>
+			<RessourceNavigation>
+				<Link to='/'>
+					<MdKeyboardDoubleArrowLeft size={25} style={{ color: 'white' }} />
+				</Link>
+			</RessourceNavigation>
+			<AnnouncementContainer>
+				<p>
+					Hier findet ihr Infos und hilfreiche Tips in Sachen Aktivismus - Gruppierungen, Bücher, Videomaterial und
+					Podcasts
+				</p>
+			</AnnouncementContainer>
 			<MasonryContainer className='images'>
-				<ResponsiveMasonry columnsCountBreakPoints={{ 250: 1, 400: 2, 900: 3 }}>
+				<ResponsiveMasonry columnsCountBreakPoints={{ 250: 1, 400: 2, 900: 4 }}>
 					<RessourceMasonryStyles gutter='10px'>
-						{array.map((item) => (
-							<RessourceCardComponent placeholder={item.toString()} />
-						))}
+						{allDataArray.length > 0 &&
+							allDataArray.map((item) => (
+								<RessourceCardComponent
+									key={item.id}
+									url={item.url as string}
+									image={item?.thumbnail?.image?.publicUrlTransformed as string}
+									name={item.name as string}
+									description={item.description as string}
+									author={item.author as string}
+								/>
+							))}
 					</RessourceMasonryStyles>
 				</ResponsiveMasonry>
 			</MasonryContainer>
 		</PageContainer>
 	);
 }
-
-export const RessourceMasonryStyles = styled(MasonryStyles)`
-	height: 100%;
-	padding: 10px;
-`;
